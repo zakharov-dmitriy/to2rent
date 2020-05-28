d7052421bafb4c3e8e7d713c3b98276cbfa0491b
Дмитрий Захаров <dz@MacBook-Pro.local>
2020-05-28 17:24:18 +0700

    added site

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(function () {
+   $('.slider').slick({
+      // fade: true,
+      nextArrow: '<button class="slick-arrow slick-next"><img src="images/next.svg" alt="next"></button>',
+      prevArrow: '<button class="slick-arrow slick-prev"><img src="images/prev.svg" alt="prev"></button>'
+      // responsive: [
+      //    {
+      //       breakpoint: 441,
+      //       settings: {
+      //          arrows: false
+      //       }
+      //    }
+      // ]
+   });
+});
+
+
